64577e799585d354718710702c4efbe0e106b9ae
alvaro <alvaromgs@correo.ugr.es>
2017-10-12 17:50:31 +0200

    Añadidos tests #4

---
+++ bot/test.py
@@ -0,0 +1,53 @@
+import unittest
+import time
+from listas import *
+
+class myTest(unittest.TestCase):
+
+    def testgetLists(self):
+        self.assertTrue(getLists(), "La lista está vacía")
+
+    def testgetList(self):
+        self.assertTrue(isinstance(getList("Vistas"), list), "No es una lista")
+
+    def testaddList(self):
+        pelis["Nolan"] = [
+                             {"Origen": 9, "Memento": 10},
+                             time.strftime("%d/%m/%Y %H:%M")
+                         ]
+        self.assertTrue("Nolan" in getLists(), "No se ha añadido la lista")
+
+    def testrenameList(self):
+        renameList("Pendientes", "Pendientesv2")
+        self.assertTrue("Pendientesv2" in getLists(), "No se ha renombrado la lista")
+
+    def testremoveList(self):
+        removeList("Favoritas")
+        self.assertTrue("Favoritas" not in getLists(), "No se ha borrado la lista")
+
+    def testgetMovies(self):
+        self.assertTrue(isinstance(getMovies("Vistas"), dict), "No es un diccionario")
+
+    def testgetRating(self):
+        self.assertTrue(isinstance(getRating("Vistas", "Cadena perpetua"), (int, float)), "No es un número")
+
+    def testgetTime(self):
+        self.assertTrue(isinstance(getTime("Vistas"), str), "No es un string")
+
+    def testupdateMovie(self):
+        nota = 9
+        updateMovie("Vistas", "Cadena perpetua", nota)
+        updateMovie("Vistas", "El truco final", nota)
+        self.assertTrue(getRating("Vistas", "Cadena perpetua") == nota, "No se ha actualizado la puntuación")
+        self.assertTrue("El truco final" in getMovies("Vistas"), "No se ha añadido la película")
+
+    def testupdateTime(self):
+        updateTime("Vistas")
+        self.assertTrue(getTime("Vistas") == time.strftime("%d/%m/%Y %H:%M"), "No se ha actualizado la fecha de modificación de la lista")
+
+    def testremoveMovie(self):
+        removeMovie("Vistas", "Cadena perpetua")
+        self.assertTrue("Cadena perpetua" not in getMovies("Vistas"), "No se ha borrado la película")
+
+if __name__ == '__main__':
+    unittest.main()
