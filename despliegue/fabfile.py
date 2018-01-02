36449a979b47ddeedaa1df7192358d6784f3179c
alvaro <alvaromgs@correo.ugr.es>
2018-01-02 14:22:19 +0100

    Añadido fichero de despliegue closes #9

---
+++ despliegue/fabfile.py
@@ -0,0 +1,15 @@
+from fabric.api import run
+
+def install():
+    run("git clone https://github.com/alvaromgs/proyectoIV-1718.git")
+    run("pip3 install -r /home/vagrant/proyectoIV-1718/requirements.txt")
+
+def uninstall():
+    run("sudo rm -R /home/vagrant/proyectoIV-1718")
+
+def update():
+    run("cd /home/vagrant/proyectoIV-1718 && git pull")
+    run("pip3 install -r /home/vagrant/proyectoIV-1718/requirements.txt")
+
+def start():
+    run("sudo python3 /home/vagrant/proyectoIV-1718/bot/app.py")
