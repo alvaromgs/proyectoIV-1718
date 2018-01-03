1fdbe997800ba125e9532dba109b0ec420f16ce6
alvaro <alvaromgs@correo.ugr.es>
2018-01-03 23:07:42 +0100

    Actualizado fabfile

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -7,10 +7,8 @@ def install():
 def uninstall():
     run("sudo rm -R /home/vagrant/proyectoIV-1718")
 
-def update():
-    run("cd /home/vagrant/proyectoIV-1718 && git pull")
-    run("pip3 install -r /home/vagrant/proyectoIV-1718/requirements.txt")
-
 def start():
-    run("python3 /home/vagrant/proyectoIV-1718/bot/bot.py")
     run("sudo python3 /home/vagrant/proyectoIV-1718/bot/app.py")
+
+def bot():
+    run("python3 /home/vagrant/proyectoIV-1718/bot/bot.py")
